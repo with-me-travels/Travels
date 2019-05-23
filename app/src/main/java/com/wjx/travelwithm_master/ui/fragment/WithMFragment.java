1b496be5f4201e363f5833abfcda2ee7d4cff333
秋天天 <1251282916@qq.com>
2019-05-23 09:06:25 +0800

    侧滑首页框架搭建哦

---
+++ app/src/main/java/com/wjx/travelwithm_master/ui/fragment/WithMFragment.java
@@ -0,0 +1,33 @@
+package com.wjx.travelwithm_master.ui.fragment;
+
+import com.wjx.travelwithm_master.R;
+import com.wjx.travelwithm_master.base.BaseFragment;
+import com.wjx.travelwithm_master.base.BasePresenter;
+import com.wjx.travelwithm_master.utils.ToastUtil;
+
+public class WithMFragment extends BaseFragment {
+    @Override
+    protected BasePresenter createPresenter() {
+        return null;
+    }
+
+    @Override
+    protected int createLayut() {
+        return R.layout.fragment_with_m;
+    }
+
+    @Override
+    public void showProgressbar() {
+
+    }
+
+    @Override
+    public void hideProgressBar() {
+
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+}
